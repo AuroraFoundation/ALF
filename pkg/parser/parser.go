c10605c21a4deae1c9e00fa0da0ac8bcfecbcf77
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-10-10 11:55:25 -0500

    implement NonNestedAttr

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"errors"
+	"fmt"
 	"io"
 
 	"github.com/AuroraFoundation/ALF/pkg/lexer"
@@ -18,7 +19,7 @@ type ALF struct {
 
 	Lyric Lyric
 
-	Notes  []string
+	Notes []string
 }
 
 type Lyric struct {
@@ -27,56 +28,150 @@ type Lyric struct {
 
 // Parser implements a source code file parser in ALF.
 type Parser struct {
-	r io.Reader
+	lex   *lexer.Lexer
+	items <-chan lexer.Item
+
+	peek lexer.Item
 }
 
 // New creates and initializes a new `Parser` structure.
 func New(r io.Reader) *Parser {
-	return &Parser{
-		r,
-	}
+	p := Parser{}
+
+	p.lex, p.items = lexer.New(r)
+
+	return &p
 }
 
 // Decode parses and returns an `ALF` structure with the parsed source code.
 func (p *Parser) Decode() (ALF, error) {
-	_, items := lexer.New(p.r)
-
-	var attrName string
 	var alf ALF
 
 	for {
-		switch item := <-items; item.Token {
+		switch item := p.nextItem(); item.Token {
 		case lexer.TokenName:
-			attrName = item.Literal
-
-		case lexer.TokenText:
-			switch attrName {
+			switch item.Literal {
 			case "Title":
-				alf.Title = item.Literal
+				alf.Title = p.parseSimpleAttrVal()
 			case "Author":
-				alf.Author = item.Literal
+				alf.Author = p.parseSimpleAttrVal()
 			case "Artist":
-				alf.Artist = item.Literal
+				alf.Artist = p.parseSimpleAttrVal()
 			case "Album":
-				alf.Album = item.Literal
+				alf.Album = p.parseSimpleAttrVal()
+			case "Names":
+				alf.Names = p.parseList(0)
+			case "Notes":
+				alf.Notes = p.parseList(0)
+			case "Lyric":
+				alf.Lyric = p.parseLyric()
 			default:
-				return ALF{}, errors.New("unknown attribute name")
-			}
-
-		case lexer.TokenList:
-			<-items // Consume space.
-			item = <-items
-
-			if attrName == "Names" {
-				alf.Names = append(alf.Names, item.Literal)
-			} else if attrName == "Notes" {
-				alf.Notes = append(alf.Notes, item.Literal)
-			} else if attrName == "Order" {
-				alf.Lyric.Order = append(alf.Lyric.Order, item.Literal)
+				return ALF{}, errors.New(fmt.Sprintf("unknown attribute name %q", item.Literal))
 			}
 
 		case lexer.TokenEOF:
 			return alf, nil
 		}
 	}
+}
+
+func (p *Parser) parseLyric() Lyric {
+	var lyric Lyric
+
+	// Consume colon.
+	p.nextItem()
+
+	indent := p.parseIndent()
+
+	for {
+		item := p.peekItem()
+
+		if item.Token == lexer.TokenEOF {
+			return lyric
+		}
+
+		p.nextItem()
+
+		if item.Token == lexer.TokenNewline {
+			if p.parseIndent() < indent {
+				return lyric
+			}
+		}
+
+		if item.Literal == "Order" {
+			lyric.Order = p.parseList(indent)
+		}
+	}
+}
+
+func (p *Parser) parseList(n int) []string {
+	var list []string
+
+	// Consume colon.
+	p.nextItem()
+
+	for {
+		item := p.peekItem()
+
+		if item.Token == lexer.TokenEOF {
+			return list
+		}
+
+		p.nextItem()
+
+		if item.Token == lexer.TokenNewline {
+			if p.parseIndent() < n {
+				return list
+			}
+
+			continue
+		}
+
+		if item.Token != lexer.TokenList {
+			return list
+		}
+
+		// Consume whitespace.
+		p.nextItem()
+
+		list = append(list, p.nextItem().Literal)
+	}
+}
+
+func (p *Parser) parseSimpleAttrVal() string {
+	// Consume colon and whitespace.
+	p.nextItem()
+	p.nextItem()
+
+	return p.nextItem().Literal
+}
+
+func (p *Parser) parseIndent() int {
+	item := p.peekItem()
+
+	if item.Token != lexer.TokenIndent {
+		return 0
+	}
+
+	p.nextItem()
+
+	return len(item.Literal)
+}
+
+func (p *Parser) nextItem() lexer.Item {
+	if p.peek == (lexer.Item{}) {
+		return <-p.items
+	}
+
+	ret := p.peek
+	p.peek = (lexer.Item{})
+	return ret
+}
+
+func (p *Parser) peekItem() lexer.Item {
+	if p.peek == (lexer.Item{}) {
+		p.peek = <-p.items
+	}
+
+	return p.peek
 }
